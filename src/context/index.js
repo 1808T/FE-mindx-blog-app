fdfb7f7b9c2d5296f91372af2600f07366860c21
1808T <nguyenanhtu18081998@gmail.com>
2022-07-19 16:01:52 +0700

    minor-fix

--- src/context/index.js
+++ src/context/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useState, createContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-require("dotenv").config();
 
 const UserContext = createContext();
 
